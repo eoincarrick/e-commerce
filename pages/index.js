7168f9a786f7caa744abb8451c057ad0bd331d81
Eoin Carrick <22carri99@gmail.com>
2022-05-12 18:10:50 +0000

    save

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import css from '../styles/Home.module.css';
 import { HiShoppingCart } from 'react-icons/hi';
-import { AiOutlineUser, AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineSearch, AiFillStar } from 'react-icons/ai';
 import shoe from '../assets/s.jpg';
 import Image from 'next/image';
 
@@ -74,11 +74,79 @@ export default function Home() {
         <section className={css.product}>
           <div className={css.productCategory}>
             <p className={css.productLane}>SHOES</p>
-            <div className={css.productCard}>
-              <Image className={css.productImage} width={300} height={200} src={shoe} alt='shoe' />
-              <div className={css.desc}>
-                <span>$323.32</span>
-                <span></span>
+            <div className={css.productCardContainer}>
+              <div className={css.productCard}>
+                <Image
+                  className={css.productImage}
+                  width={300}
+                  height={200}
+                  src={shoe}
+                  alt='shoe'
+                />
+                <div className={css.desc}>
+                  <span className={css.name}>Cotton Sneakers</span>
+                  <span>
+                    {[
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                    ]}
+                  </span>
+                  <span className={css.price}>$89.00</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={css.productCategory}>
+            <p className={css.productLane}>WATCHES</p>
+            <div className={css.productCardContainer}>
+              <div className={css.productCard}>
+                <Image
+                  className={css.productImage}
+                  width={300}
+                  height={200}
+                  src={shoe}
+                  alt='shoe'
+                />
+                <div className={css.desc}>
+                  <span className={css.name}>Cotton Sneakers</span>
+                  <span>
+                    {[
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                    ]}
+                  </span>
+                  <span className={css.price}>$89.00</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={css.productCategory}>
+            <p className={css.productLane}>GLASSES</p>
+            <div className={css.productCardContainer}>
+              <div className={css.productCard}>
+                <Image
+                  className={css.productImage}
+                  width={300}
+                  height={200}
+                  src={shoe}
+                  alt='shoe'
+                />
+                <div className={css.desc}>
+                  <span className={css.name}>Cotton Sneakers</span>
+                  <span>
+                    {[
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                      <AiFillStar className={css.star} />,
+                    ]}
+                  </span>
+                  <span className={css.price}>$89.00</span>
+                </div>
               </div>
             </div>
           </div>
