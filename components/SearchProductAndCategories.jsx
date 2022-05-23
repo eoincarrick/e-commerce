5af035acc214d277519b3bde2dda943e8aaeea61
Eoin Carrick <22carri99@gmail.com>
2022-05-23 22:52:32 +0000

    search

--- components/SearchProductAndCategories.jsx
+++ components/SearchProductAndCategories.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import css from '../styles/SearchProductAndCategories.module.css';
+import { urlFor } from '../library/client';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
 
 const SearchProductAndCategories = ({
   data,
@@ -9,6 +12,25 @@ const SearchProductAndCategories = ({
   const [filteredName, setFilteredName] = useState([]);
   const [filteredCategory, setFilteredCategory] = useState([]);
   const [word, setWord] = useState('');
+
+  const handleSearch = (event) => {
+    const searchWord = event.target.value.toLowerCase();
+    setWord(searchWord);
+    const searchName = data.filter((productName, index) => {
+      return productName.name.toLowerCase().includes(searchWord);
+    });
+
+    const searchCategory = data.filter((productName, index) => {
+      return productName.name.toLowerCase().includes(searchWord);
+    });
+
+    if (searchWord === '') {
+      setFilteredName([]);
+      setFilteredCategory([]);
+    } else {
+      setFilteredName(searchName);
+    }
+  };
 
   const handleClear = () => {
     setFilteredName([]);
@@ -20,44 +42,57 @@ const SearchProductAndCategories = ({
     case 'name':
       return (
         <div className={css.container}>
-          <div className={css.input}>
+          <div className={css.inputContainer}>
             <input
+              className={css.input}
               type='text'
               placeholder=''
               value={word}
-              onChange={(event) => setWord(event.target.value)}
+              onChange={handleSearch}
             />
-            <FaTimes onClick={handleClear} /> <FaSearch />
+            <FaTimes
+              onClick={handleClear}
+              className={`${css.icons} ${css.icon}`}
+            />{' '}
+            <FaSearch className={css.icons} />
           </div>
 
-          <ul className={css.display}>
-            {data.map((item, index) => (
-              <li key={i} className={css.card}>
-                <div className={css.center}>
-                  <img
-                    className={css.productImage}
-                    src={urlFor(item?.image[0])}
-                    alt={item.slug.current}
-                    loading='lazy'
-                  />
-                  <p className={css.productName}>{item.name}</p>
-                </div>
-                <ul className={css.productDetails}>
-                  <li className={css.priceContainer}>
-                    <span className={css.green}>${item.new_price}</span>
-                    <span className={css.gray}>${item.old_price}</span>
-                  </li>
-                  <li className={css.buttonContainer}>
-                    <Link href={`/product/${item.slug.current}`}>
-                      <button className={css.btn} type='button'>
-                        Details
-                      </button>
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            ))}
-          </ul>
+          {filteredName !== 0 && (
+            <ul className={css.cardContainer}>
+              {filteredName.map((item, i) => (
+                <li key={i} className={css.card}>
+                  <div className={css.center}>
+                    <img
+                      className={css.productImage}
+                      src={urlFor(item?.image[0])}
+                      alt={item.slug.current}
+                      loading='lazy'
+                    />
+                    <p className={css.productName}>{item.name}</p>
+                  </div>
+                  <ul className={css.productDetails}>
+                    <li className={css.priceContainer}>
+                      <span className={css.green}>${item.new_price}</span>
+                      <span className={css.gray}>${item.old_price}</span>
+                    </li>
+                    <li className={css.buttonContainer}>
+                      <Link href={`/product/${item.slug.current}`}>
+                        <button className={css.btn} type='button'>
+                          Details
+                        </button>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          )}
+
+          {!filteredName && (
+            <div>
+              <p>Search For A Product Name</p>
+            </div>
+          )}
         </div>
       );
       break;
@@ -65,18 +100,19 @@ const SearchProductAndCategories = ({
     case 'category':
       return (
         <div className={css.container}>
-          <div className={css.input}>
+          <div className={css.inputContainer}>
             <input
+              className={css.input}
               type='text'
               placeholder=''
               value={word}
-              onChange={(event) => setWord(event.target.value)}
+              onChange={handleSearch}
             />
             <FaTimes onClick={handleClear} /> <FaSearch />
           </div>
 
-          <ul className={css.display}>
-            {data.map((item, index) => (
+          <ul className={css.cardContainer}>
+            {data.map((item, i) => (
               <li key={i} className={css.card}>
                 <div className={css.center}>
                   <img
