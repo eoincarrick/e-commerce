9b004d570332416c29e685a293965e8ba25e3afe
Eoin Carrick <22carri99@gmail.com>
2022-05-29 20:30:58 +0000

    save

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
-export { default as Layout } from './layout';
+export { default as Layout } from './Layout';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
 export { default as Cart } from './Cart';
